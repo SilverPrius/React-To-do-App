6361f13d87c3fd9542bb7d55a69fd7dceefe493f
Long Hoang <long88hoang@gmail.com>
2023-01-24 13:26:30 -0600

    Updated Link in Todos.js

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,30 +1,28 @@
 import { getTodos } from "../services/todos-api"
-import { useState, useEffect } from 'react'
+import{useState, useEffect} from 'react'
 import Create from "./CreateTodo"
+import { Link } from "react-router-dom"
 
 export default function Todos() {
     const [todos, setTodos] = useState([])
     useEffect(() => {
         getTodos() // calling the function to get the data
-            .then(res => setTodos(res.data)) // setting state with returned data
+        .then(res => setTodos(res.data)) // setting state with returned data
     }, [])
     console.log(todos)
-    return (
+    return(
         <div>
-            <ul>
-                {todos.map((todo) => {
-                    return (
-                        <li>
-                            <a href={`/${todo._id}`}>
-                                <h3 className={`${todo.complete ? "completed" : "notCompleted"}`}>
-                                    {todo.description}
-                                </h3>                                
-                            </a>
-                        </li>
-                    )
-                })}
-            </ul>
-            <Create />
+            <>
+            {todos.map((todo) =>{
+                return (
+                    <div>
+                    <Link to={`/${todo._id}`}>{todo.description}</Link>
+                    </div>
+                    // <li><a href={`/${todo._id}`}>{todo.description}</a></li>
+                )
+            })}
+          </>
+          <Create />  
         </div>
     )
 }
